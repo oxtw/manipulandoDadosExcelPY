30e9cac5d0c88283af109c38e7391a3db9a6c0f6
oxtw <weigertmiguel@hotmail.com>
2023-08-07 18:20:44 -0300

    Retornando todos os campos da tabela, no formato especifico pedido

--- tarefa.py
+++ tarefa.py
@@ -1,24 +1,48 @@
 import openpyxl
 
-# Defina o caminho do arquivo Excel ou substitua pelo caminho do seu arquivo
-caminho_arquivo = 'completo_01-08-2023.xlsx'
+# Carregar o arquivo Excel
+workbook = openpyxl.load_workbook('tabelas\completo_01-08-2023.xlsx')
+sheet = workbook.active
 
-# Carregue o arquivo Excel usando a função load_workbook do openpyxl
-arquivo_excel = openpyxl.load_workbook(caminho_arquivo)
+# Dicionário para armazenar os dados filtrados por link2
+filtered_data = {}
 
-# Escolha a planilha que deseja ler (caso haja várias planilhas no arquivo)
-nome_planilha = 'Report'
-planilha = arquivo_excel[nome_planilha]
+# Iterar pelas linhas da planilha (começando da segunda linha, assumindo que a primeira linha é o cabeçalho)
+for row in sheet.iter_rows(min_row=2, values_only=True):
+    codigo = row[0]
+    criado_em = row[8]
+    link2 = row[23]
+    if link2 not in filtered_data:
+        filtered_data[link2] = []
+    
+    # Verificar se o chamado é do mês 07/2023
+    if criado_em.month == 7 and criado_em.year == 2023:
+        chamado = {
+            'Código': codigo,
+            'Criado em': criado_em,
+            'Título do problema': row[16],
+            'Descrição do problema': row[17],
+            'Data e hora que retornou ao normal': row[30],
+            'Observações finais': row[31],
+            'Laudo Final': row[54],
+            'Tempo de atraso do cliente em horas': row[58],
+            'Tempo de atraso do cliente em minutos': row[59],
+            'Justificativa do atraso do cliente': row[61]
+        }
+        filtered_data[link2].append(chamado)
 
-# Verifique se a coluna 'Códigos' existe na planilha
-coluna_codigos = planilha['A']  # Supondo que a coluna 'Códigos' esteja na coluna C
-
-if coluna_codigos:
-    # Acesse os valores da coluna 'Códigos' e imprima-os
-    codigos = [celula.value for celula in coluna_codigos if celula.value is not None]
-
-    # Imprima os itens da coluna 'Códigos'
-    print(codigos)
-else:
-    print("A coluna 'Códigos' não existe na planilha.")
+# Imprimir os resultados formatados
+for link2, chamados in filtered_data.items():
+    print(f"{link2}")
+    for chamado in chamados:
+        print(f"\tCódigo {chamado['Código']} (relacionado ao {link2}):")
+        print(f"\t\tCriado em: {chamado['Criado em']}")
+        print(f"\t\tTítulo do problema: {chamado['Título do problema']}")
+        print(f"\t\tDescrição do problema: {chamado['Descrição do problema']}")
+        print(f"\t\tData e hora que retornou ao normal: {chamado['Data e hora que retornou ao normal']}")
+        print(f"\t\tObservações finais: {chamado['Observações finais']}")
+        print(f"\t\tLaudo Final: {chamado['Laudo Final']}")
+        print(f"\t\tTempo de atraso do cliente em horas: {chamado['Tempo de atraso do cliente em horas']}")
+        print(f"\t\tTempo de atraso do cliente em minutos: {chamado['Tempo de atraso do cliente em minutos']}")
+        print(f"\t\tJustificativa do atraso do cliente: {chamado['Justificativa do atraso do cliente']}")
 
